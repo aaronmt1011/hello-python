97b77fd65baf97af0fb9420af8b9dc53f874899a
aaronmt1011 <aaronmt1011@gmail.com>
2023-03-27 15:43:35 -0600

    Practiced basic python

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+print(2 + 3)
+print("2 + 3 =", 2 + 3)
+
+def hello() :
+    print("....")
+    print("Hello")
+    print("Coooolllll!")
+    print("....")
+
+hello()
+
+
+def greet(person):
+    print("Hello", person)
+    print("How are you?")
+
+greet("Mugen")
